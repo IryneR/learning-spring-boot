b830f3b4821371aaca64a8e9f6b0c025a63159e1
Iryna <irishka2991@gmail.com>
2020-06-05 17:29:17 +0300

    Add the Service layer

--- src/main/java/com/snailtraveller/learningspringboot/dao/UserDao.java
+++ src/main/java/com/snailtraveller/learningspringboot/dao/UserDao.java
@@ -3,12 +3,13 @@ package com.snailtraveller.learningspringboot.dao;
 import com.snailtraveller.learningspringboot.model.User;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserDao {
-    List<User> getAllUsers();
-    User getUser(UUID userUid);
-    int updateUser(UUID userUid, User user);
-    int removeUser(UUID userUid);
+    List<User> selectAllUsers();
+    Optional<User> selectUserByUserUid(UUID userUid);
+    int updateUser( User user);
+    int deleteUserByUserUid(UUID userUid);
     int insertUser(UUID userUid, User user);
 }
